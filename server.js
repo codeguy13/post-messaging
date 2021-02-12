da20f491020769f11316fa80b75bc248b9407add
codeguy13 <bensilverstein@protonmail.com>
2021-02-12 21:11:37 +0000

    Worked on the posting

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 var messages = [
-    {name: 'Tim', message: 'Hi!'},
-    {name: 'Bob', message: 'Hello!'}
-];
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
 
-app.get('/messages', (req, res) => {
-    res.send(messages);
-});
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+})
 
-app.post('/messages', (req, res) => {
-    console.log(req.body);
-    res.sendStatus(200);
-});
+app.post('/messages', (req, res) =>{
+    messages.push(req.body)
+    res.sendStatus(200)
+})
 
 var server = app.listen(3000, () => {
-    console.log('Server is listening on port', server.address().port);
-});
+    console.log('server is listening on port', server.address().port)
+})
